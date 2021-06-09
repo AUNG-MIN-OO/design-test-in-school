6f5647be3491da28ff6e9d17f277963d615f4423
Aung Min Oo <aungminoo1314@gmail.com>
2021-06-09 12:41:17 +0900

    animation end

--- webpage4_responsiveBlog_with_animations/js/main.js
+++ webpage4_responsiveBlog_with_animations/js/main.js
@@ -1,3 +1,17 @@
+const responsive  ={
+    0:{
+        items:1
+    },
+    320:{
+        items:1
+    },
+    560:{
+        items: 2
+    },
+    960:{
+        items:3
+    },
+}
 $(document).ready(function(){
 
     $nav = $('.nav');
@@ -16,5 +30,16 @@ $('.owl-carousel').owlCarousel({
     autoplay:true,
     autoplayTimeout: 3000,
     nav: true,
-    navText: [$('.owl-navigation .owl-nav-prev'),$('.owl-navigation .owl-nav-next')]
+    navText: [$('.owl-navigation .owl-nav-prev'),$('.owl-navigation .owl-nav-next')],
+    responsive: responsive
 });
+
+//click to scroll up
+$('.move-up span').click(function (){
+    $('html,body').animate({
+        scrollTop:0
+    },1000)
+})
+
+//aos library
+AOS.init();
